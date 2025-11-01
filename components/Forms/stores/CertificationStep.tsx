5e0e802e63996f26a763b388e464d02d1d8d6639
Collins <officialrrye5@gmail.com>
2025-11-01 23:27:49 +0100

    fix api

--- components/Forms/stores/CertificationStep.tsx
+++ components/Forms/stores/CertificationStep.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { useFormContext } from 'react-hook-form';
-import { Input } from '@/components/ui/Input';
-import { FileUpload } from '@/components/Forms/FileUpload';
+import {useFormContext} from 'react-hook-form';
+import {Input} from '@/components/ui/input';
+import {FileUpload} from '@/components/Forms/FileUpload';
 
 export const CertificationStep = () => {
-  const { register } = useFormContext();
+    const {register} = useFormContext();
 
-  return (
-    <div className="space-y-6">
-      <Input label="Official Business Name" {...register('certification.businessName')} />
-      <Input label="CAC Number" {...register('certification.cacNumber')} />
+    return (
+        <div className="space-y-6">
+            <Input label="Official Business Name" {...register('certification.businessName')} />
+            <Input label="CAC Number" {...register('certification.cacNumber')} />
 
-      <FileUpload label="Certificate of Incorporation" {...register('certification.incorporationCertificate')} />
-      <FileUpload label="Status Report" {...register('certification.statusReport')} />
-    </div>
-  );
+            <FileUpload label="Certificate of Incorporation" {...register('certification.incorporationCertificate')} />
+            <FileUpload label="Status Report" {...register('certification.statusReport')} />
+        </div>
+    );
 };
